8a67e347796ce4e5745fc0331bb6b0ff14bbdc23
Andre Rovira <oscarandrerovira@hotmail.com>
2025-05-31 19:03:42 -0600

    Remove unused methods from IMeetingRepository and MeetingRepository to streamline the interface and implementation.

--- src/EnterpriseScheduler/Interfaces/IMeetingRepository.cs
+++ src/EnterpriseScheduler/Interfaces/IMeetingRepository.cs
@@ -9,8 +9,4 @@ public interface IMeetingRepository
     Task<Meeting> AddAsync(Meeting meeting);
     Task UpdateAsync(Meeting meeting);
     Task DeleteAsync(Guid id);
-
-    Task<IEnumerable<Meeting>> GetMeetingsForUserAsync(Guid userId, DateTime startTime, DateTime endTime);
-    Task<bool> HasTimeConflictAsync(Meeting meeting);
-    Task<IEnumerable<DateTime>> FindAvailableSlotsAsync(IEnumerable<Guid> participantIds, DateTime startTime, DateTime endTime, TimeSpan duration);
 }
