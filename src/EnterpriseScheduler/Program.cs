c9a43790cf0ff8c47f7f102ef266c3dbeb63bc08
Andre Rovira <oscarandrerovira@hotmail.com>
2025-05-31 19:30:05 -0600

    Add PostgreSQL connection string and configure DbContext in Program.cs

--- src/EnterpriseScheduler/Program.cs
+++ src/EnterpriseScheduler/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using EnterpriseScheduler.Data;
+using EnterpriseScheduler.Repositories;
+using EnterpriseScheduler.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IMeetingRepository, MeetingRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
